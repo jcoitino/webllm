e3d25e5a1d27437d3b28fdda8c6d2ab9e8e9f364
Javier Coitino <jcoitino@infragistics.com>
2025-04-07 16:37:29 -0300

    Remove left behind files

--- src/app/app.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from '@open-wc/testing';
-import App from './app.js';
-
-describe('App', () => {
-  it('<app-root> is an instance of App', async () => {
-    const element = document.createElement('app-root');
-    expect(element).to.be.instanceOf(App);
-  });
-});
